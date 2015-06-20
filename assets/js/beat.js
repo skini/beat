36e98e8d0a12f459975ec237d3d9b3d8704bd928
nancywong <nancypwong@gmail.com>
2015-06-20 18:43:23 -0400

    generate tags based on array in beat.js

--- assets/js/beat.js
+++ assets/js/beat.js
@@ -3,6 +3,7 @@
 //Janita Chalam <janitachalam@gmail.com>
 //Shloka Kini <srk@shlokakini.com>
 
+// DATA
 var songs_yt_links = ["https://www.youtube.com/embed/jSXiNdTbTA4",
 		"https://www.youtube.com/embed/uYsq7fbRbvk",
 		"https://www.youtube.com/embed/2uVHNib1uzE",
@@ -11,7 +12,8 @@ var songs_yt_links = ["https://www.youtube.com/embed/jSXiNdTbTA4",
 		"https://www.youtube.com/embed/sfTa_NLiXRU",
 		"https://www.youtube.com/embed/wZcNacuLSGQ",
 		"https://www.youtube.com/embed/2eq6l9P8Wf0",
-		"https://www.youtube.com/embed/nfQJRtf0kr4", 
+		"https://www.youtube.com/embed/nfQJRtf0kr4",
+        "https://www.youtube.com/embed/2BtUVKYxfI8",
 		"https://www.youtube.com/embed/EZfDUdGWRhQ"];
 
 
@@ -26,27 +28,56 @@ var talks_poems_links = ["https://www.youtube.com/embed/LkK2fwZfVjA",
 "https://www.youtube.com/embed/V-bjOJzB7LY", 
 "https://www.youtube.com/embed/LBSUTPftN9E"];
 
-		console.log(songs_yt_links);
-		console.log(talks_poems_links);
+var tags = [
+    'love',
+    'friendship',
+    'joy',
+    'surprise',
+    'trust',
+    'envy',
+    'fear',
+    'anger',
+    'sadness',
+    'pity',
+    'disgust',
+    ];
 
-		function generateSong(){
+console.log(songs_yt_links);
+console.log(talks_poems_links);
 
-			var index = getRandomInt(0,10);
-			var index2 = getRandomInt(0,10);
-			var songURL = songs_yt_links[index];
-			var poemURL = talks_poems_links[index2];
-			var songiFrame = $('.song_iframe');
-			var poemiFrame = $('.poem_iframe');
-			$(".song_iframe").attr('src', songURL+'?autoplay=1');
-			$(".poem_iframe").attr('src', poemURL+'?autoplay=1');
-		}
+// FUNCTIONS
+function generateSong(){
 
-        toggleTag = function(id) {
-            var $tag = $('#' + id);
-            $tag.toggleClass('selected deselected');
-            console.log('hello?');
-        }
+    var index = getRandomInt(0,songs_yt_links.length);
+    var index2 = getRandomInt(0,talks_poems_links.length);
+    var songURL = songs_yt_links[index];
+    var poemURL = talks_poems_links[index2];
+    var songiFrame = $('.song_iframe');
+    var poemiFrame = $('.poem_iframe');
+    $(".song_iframe").attr('src', songURL+'?autoplay=1');
+    $(".poem_iframe").attr('src', poemURL+'?autoplay=1');
+}
+
+toggleTag = function(id) {
+    var $tag = $('#' + id);
+    $tag.toggleClass('selected deselected');
+    console.log('hello?');
+}
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+generateTag = function(tagName) {
+    var htmlString = "<input class='btn tag deselected' id='" + tagName + "' type='button' onclick='toggleTag(this.id)' value='" + tagName + "'/>"
+
+    $('.tags').append(htmlString);
+}
+
+generateTags = function() {
+    for (tag of tags) {
+        generateTag(tag);
+    }
+}
+
+generateTags();
