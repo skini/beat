cc5f9b10fedab410a42aa60f4b5d9ecb2f20ecf3
skini <skini@mit.edu>
2015-06-20 15:22:22 -0400

    skeleton code for frontend

---
+++ mh.js
@@ -0,0 +1,15 @@
+//Authors 
+//Nancy Wong <nancypwong@gmail.com>
+//Janita Chalam <janitachalam@gmail.com>
+//Shloka Kini <srk@shlokakini.com>
+
+songs_yt_links = [];
+
+talks_poems_links = [];
+
+generateSong = function(){
+	
+}
+
+
+//javascript stuff
