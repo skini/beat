03f387a1ba9bb20d625f1a1527b4d1129d713d8c
skini <skini@mit.edu>
2015-06-20 15:48:26 -0400

    added new theme, added songs and poems to list

--- mh.js
+++ mh.js
@@ -3,9 +3,9 @@
 //Janita Chalam <janitachalam@gmail.com>
 //Shloka Kini <srk@shlokakini.com>
 
-songs_yt_links = [];
+songs_yt_links = ["https://www.youtube.com/watch?v=jSXiNdTbTA4", "https://www.youtube.com/watch?v=uYsq7fbRbvk", "https://www.youtube.com/watch?v=2uVHNib1uzE"];
 
-talks_poems_links = [];
+talks_poems_links = ["https://www.youtube.com/watch?v=LkK2fwZfVjA", "https://www.youtube.com/watch?v=cWPx9UyEdYw", "https://www.youtube.com/watch?v=B3wsSdPzAuQ", "https://www.youtube.com/watch?v=H_8y0WLm78U", "https://www.youtube.com/watch?v=hg3umXU_qWc", "https://www.youtube.com/watch?v=6X9nRnKshCM", "https://www.youtube.com/watch?v=0kKh1es5vBQ"];
 
 generateSong = function(){
 	
